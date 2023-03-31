61a28b785ed4805e7a3c6aabfb7175126cefa6e0
Marknjo <marknjo7@gmail.com>
2023-03-31 17:25:43 +0300

    feat(HomePageNav): clear todo Implement active link

--- apps/next-app/src/components/navigation/NavLink.tsx
+++ apps/next-app/src/components/navigation/NavLink.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 import { Link, Box } from '@chakra-ui/react'
 import { NavItem } from './NavItem.interface'
 
-/* @TODO: Implement active link */
+const activeState = {
+  color: 'teal.100',
+  borderBottomColor: 'teal.100',
+  transform: 'scaleX(1.02)',
+}
 
 const NavLink = ({ title, link }: NavItem) => {
-  const activeState = {
-    color: 'teal.100',
-    borderBottomColor: 'teal.100',
-    transform: 'scaleX(1.02)',
-  }
-  console.log('Call')
+  const params = useRouter()
 
   return (
     <Box as="li">
@@ -21,7 +21,8 @@ const NavLink = ({ title, link }: NavItem) => {
         as={NextLink}
         colorScheme="teal"
         color="teal.50"
-        fontSize="lg"
+        fontSize="md"
+        fontWeight="thin"
         paddingX="2"
         display="flex"
         alignItems="center"
@@ -29,9 +30,14 @@ const NavLink = ({ title, link }: NavItem) => {
         borderBottom="4px solid transparent"
         transition="all 150ms ease-in"
         _hover={activeState}
-        _activeLink={activeState}
+        _active={activeState}
+        _selected={activeState}
+        aria-selected={
+          `/${link}` === params.asPath ||
+          (title.toLowerCase() === 'home' && params.asPath === '/')
+        }
       >
-        {title}
+        {title.toLocaleUpperCase()}
       </Link>
     </Box>
   )
