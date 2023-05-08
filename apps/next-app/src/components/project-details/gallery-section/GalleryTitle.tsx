19842b78f074831eaf2ecdb4f7f8c581235f7cdb
Marknjo <marknjo7@gmail.com>
2023-05-08 17:09:32 +0300

    fix(project-details): gallery section main title sub-title not wrapping

--- apps/next-app/src/components/project-details/gallery-section/GalleryTitle.tsx
+++ apps/next-app/src/components/project-details/gallery-section/GalleryTitle.tsx
@@ -2,11 +2,11 @@ import { GridItem } from '@chakra-ui/react'
 import { LargeTitle } from 'ui'
 
 const GalleryTitle = () => (
-  <GridItem gridColumn="content-start/content-end" zIndex="2">
+  <GridItem gridColumn="content-start/span 4" zIndex="2">
     <LargeTitle
       title="Gallery"
       subTitle="A Collection Of the Project Sections Screenshots"
-      subTitleConfig={{ color: 'orange.100' }}
+      subTitleConfig={{ color: 'orange.100', lineHeight: 1.2 }}
       titleConfig={{ color: 'orange.50', mb: '0' }}
       alignTitle={{ alignItems: 'left' }}
     />
