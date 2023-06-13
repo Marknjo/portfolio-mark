b318f4bee01ae8f4bfde5676d243fa384774141a
Marknjo <marknjo7@gmail.com>
2023-06-13 11:07:43 +0300

    style(fix): page wrapper animations

--- apps/next-app/src/components/next-ui/PageWrapper.tsx
+++ apps/next-app/src/components/next-ui/PageWrapper.tsx
@@ -4,17 +4,22 @@ import { chakra, shouldForwardProp } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 const WrapperBox = chakra(motion.div, {
+  baseStyle: {
+    overflowX: 'hidden',
+  },
   shouldForwardProp: prop => isValidMotionProp(prop) || shouldForwardProp(prop),
 })
 
 export function PageWrapper({ children }: { children: ReactNode }) {
   return (
-    <WrapperBox initial={{ x: -1000 }} animate={{ x: 0 }} exit={{ x: 1000 }}>
+    <WrapperBox
+      initial={{ x: -1000, opacity: 0.4 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -1000, opacity: 0.4 }}
+      // @ts-ignore no problem in operation, although type error appears.
+      transition={{ type: 'tween', ease: 'easeIn', duration: 0.8 }}
+    >
       {children}
     </WrapperBox>
   )
 }
-
-// f PageWrapper = ({children}:{children: ReactNode}) => (<WrapperBox  exit={{
-//   x: 1000,
-// }}>{children}</WrapperBox>)
