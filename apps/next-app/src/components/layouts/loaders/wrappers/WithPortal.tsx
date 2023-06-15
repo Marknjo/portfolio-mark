f2f8756a43682737f66ce65fbcd19de0cdc80042
Marknjo <marknjo7@gmail.com>
2023-06-15 15:44:19 +0300

    style(loader): bing back BgWithIcons component for portal loader

--- apps/next-app/src/components/layouts/loaders/wrappers/WithPortal.tsx
+++ apps/next-app/src/components/layouts/loaders/wrappers/WithPortal.tsx
@@ -1,10 +1,23 @@
-import { Portal } from '@chakra-ui/react'
+import { Portal, SystemStyleObject } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import { BgWithICons } from 'ui'
 import WithoutPortal from './WithoutPortal'
+
+const commonStyles: SystemStyleObject = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+  zIndex: 'tooltip',
+  bgColor: 'whiteAlpha.700',
+}
 
 export default function WithPortal({ children }: { children: ReactNode }) {
   return (
     <Portal appendToParentPortal>
+      <BgWithICons sx={commonStyles} />
       <WithoutPortal>{children}</WithoutPortal>
     </Portal>
   )
