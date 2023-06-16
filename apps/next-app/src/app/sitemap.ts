254ff806f9e6a7ef2125bb892afcabbe9acc27f5
Marknjo <marknjo7@gmail.com>
2023-06-16 07:12:22 +0300

    refactor(sitemap): generation of projects maps & url fix

--- apps/next-app/src/app/sitemap.ts
+++ apps/next-app/src/app/sitemap.ts
@@ -1,15 +1,28 @@
+import { env } from 'process'
 import { getProjectNames } from '@data/utils/projectsHelpers'
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+const domain =
+  env.APP_DOMAIN ||
+  env.VERCEL_URL ||
+  env.NEXT_PUBLIC_VERCEL_URL ||
+  'http://localhost:3005'
+
+function generateProjectsSitemaps() {
   const appPages = getProjectNames()
 
-  const domain = process.env.APP_DOMAIN || '/'
-
   const projectsMaps: MetadataRoute.Sitemap = appPages.map(projectSlug => ({
-    url: `${domain === '/' ? '' : domain}/${projectSlug}`,
+    url: `${
+      domain === 'http://localhost:3005' ? '' : `https://${domain}`
+    }/${projectSlug}`,
     lastModified: new Date(),
   }))
+
+  return projectsMaps
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const projectsMaps = generateProjectsSitemaps()
 
   return [
     {
